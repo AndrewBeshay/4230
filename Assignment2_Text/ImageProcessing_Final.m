4dfd2e0532a591bc58b02f70821e48df42742887
Swapn1l <41497704+Swapn1l@users.noreply.github.com>
2019-08-27 16:26:37 +1000

    Update ImageProcessing_Final.m

--- Assignment2_Text/ImageProcessing_Final.m
+++ Assignment2_Text/ImageProcessing_Final.m
@@ -1,27 +1,30 @@
 %% Load in the Picture 
 %Adding a Live Feed thingo 
-I = imread("ImageFour.jpg");
+I = imread("sup.jpg");
 %% Main function Part
 %-----------------Initial function to filter out the boxes----------------
 [BWoverlay,S] = BoxFilter(I);
 %------------------Finding the text Area----------------------------------
+
+%% 
 [Pic] = convertBinImage2RGB(BWoverlay);
 I = rgb2gray(Pic);
 [I] = DetectText(I,Pic);
+%% 
 %-------Final Filtering of the image to get rid of everything except the text---------------- 
 I = ~I;
 [BWoverlay,S2] = BoxFilter(I);
 %--------------------Show Final Filtered Image---------------------------- 
 figure(25);
 imshow(BWoverlay);
+%% 
 %---------------------Detecting the Bold Letters--------------------------
 [mserReg,mserStat] = DetectBold(BWoverlay);
-
-
 %------------------Function to Smoothen out the Image---------------------
 binaryImage = SmoothImage(BWoverlay);
 
 %---------------Finding the Path of the robot----------------------------
+%% 
 
 boundaries = FindPath(binaryImage);
 BoundaryFilt = [];
@@ -30,12 +33,13 @@ for i = 1:size(boundaries,1)
        BoundaryFilt = [BoundaryFilt;boundaries(i)]; 
     end   
 end
+%% 
 
 %---------------Creating the Structs--------------------------------------
 txtStructs = CreateStructs(BoundaryFilt,mserReg,mserStat);
 
 %---------------Plotting the path----------------------------------------- 
-b2 = txtStructs(8).points;
+b2 = txtStructs(1).points;
 x = b2(:,2);
 y = b2(:,1);
 figure(67);
@@ -57,8 +61,8 @@ for i = 1:size(BoundaryFilt,1);
     for j = 1:size(mserStat,1)
         %CurrBox = mserStat(j).BoundingBox;
          CurrBox = mserReg(j).PixelList;
-        Cond = (find(temp(5,1) == CurrBox(:,2)));
-         Cond2 =(find(temp(5,2) == CurrBox(:,1)));
+        Cond = (find(temp(5,1) == CurrBox(:,1)));
+         Cond2 =(find(temp(5,2) == CurrBox(:,2)));
 %         Cond = (find(temp(1,1) > CurrBox(2) && temp(1,1) < CurrBox(2) +CurrBox(4)));
 %         Cond2 = (find(temp(1,2) > CurrBox(1) && temp(1,2) < CurrBox(1) +CurrBox(3)));
         
@@ -82,7 +86,6 @@ s = regionprops(~BW,'Area','BoundingBox','Centroid');
 S = s;
 corners = [];
    for i = 1:numel(s)
-  %a = find (s(i).Area < 6500 && s(i).Area > 5000 && abs(s(i).BoundingBox(3)-s(i).BoundingBox(4)) < 5);
   a = find (s(i).Area < 70 && s(i).Area > 0 | (s(i).Area < 3000)) %&& s(i).Area > 0 && abs(s(i).BoundingBox(3)-s(i).BoundingBox(4)) < 50));
    if (a == 1)
         corners = [corners;s(i).BoundingBox];
@@ -96,8 +99,6 @@ BWoverlay =  imcomplement(BW);
     BWoverlay(corners(i,2):(corners(i,2) + corners(i,4)),...
         corners(i,1):(corners(i,1) + corners(i,3)))=0;
    end    
-% figure(20)
-% imshow(BWoverlay);
 end 
 
 %% Detecting the Text
@@ -170,13 +171,13 @@ end
 mserRegions(strokeWidthFilterIdx) = [];
 mserStats(strokeWidthFilterIdx) = [];
 
-% % Show remaining regions
-% figure(25)
-% imshow(I)
-% hold on
-% plot(mserRegions, 'showPixelList', true,'showEllipses',false)
-% title('After Removing Non-Text Regions Based On Stroke Width Variation')
-% hold off
+% Show remaining regions
+figure(35)
+imshow(I)
+hold on
+plot(mserRegions, 'showPixelList', true,'showEllipses',false)
+title('After Removing Non-Text Regions Based On Stroke Width Variation')
+hold off
 regionImage = mserStats(3).Image;
 regionImage = padarray(regionImage, [1 1]);
 % Compute the stroke width image.
@@ -271,8 +272,8 @@ aspectRatio = w./h;
 % may need to be tuned for other images.
 filterIdx = aspectRatio' > 4; 
 filterIdx = filterIdx | [mserStats2.Eccentricity] > .95 ;
-filterIdx = filterIdx | [mserStats2.Solidity] < 0.55 | [mserStats2.Solidity] > 0.85 ;
-filterIdx = filterIdx | [mserStats2.Extent] < 0.2 | [mserStats2.Extent] > 0.9;
+filterIdx = filterIdx | [mserStats2.Solidity] < 0.52 | [mserStats2.Solidity] > 0.85 ;
+filterIdx = filterIdx | [mserStats2.Extent] < 0.2 | [mserStats2.Extent] > 0.8;
 filterIdx = filterIdx | [mserStats2.EulerNumber] < -4;
 
 % Remove regions
@@ -288,7 +289,7 @@ figure(32)
 imshow(BWoverlay)
 hold on
 plot(mserRegions2, 'showPixelList', true,'showEllipses',false)
-title('After Removing Non-Text Regions Based On Geometric Properties')
+%title('After Removing Non-Text Regions Based On Geometric Properties')
 hold off
 
 % Show remaining regions
@@ -296,7 +297,7 @@ figure(45)
 imshow(BWoverlay)
 hold on
 plot(mserRegions2, 'showPixelList', true,'showEllipses',false)
-title('After Removing Non-Text Regions Based On Stroke Width Variation')
+%title('After Removing Non-Text Regions Based On Stroke Width Variation')
 hold off
 
 
@@ -319,9 +320,12 @@ function boundaries = FindPath(binaryImage);
 BW3 = bwmorph(binaryImage,'thin',inf);
 BW3 = bwmorph(BW3,'clean');
 BW3 = bwmorph(BW3,'spur',4);
+
+figure(100);
+imshow(BW3);
 % se = strel('line',5,0);
 % BW3 = imdilate(BW3,se);
-boundaries = bwboundaries(BW3);
+boundaries = bwboundaries(BW3,'noholes');
 end 
 %% Function to convert Binary to RGB Image
 
